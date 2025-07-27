0b94bae800e82ec7ddecfe954f9bf5a61c44f985
yggdrasilGit <168605322+yggdrasilGit@users.noreply.github.com>
2025-07-27 11:43:17 -0300

    criando requiremenst.txt

--- setup/urls.py
+++ setup/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.documentation import include_docs_urls
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -18,10 +20,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # seu app da Company, por exemplo
+   path('admin/', admin.site.urls),
+   path('api/', include('api.urls')),  # seu app da Company, por exemplo
 
-
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   #path('docs/', include_docs_urls(title='Documentação da API')),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
